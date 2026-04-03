7d342e61ed859d1ba62bc4db2f32ebbe9a9f239b
Abdellah Hammouch <hammouchabdellah4529@gmail.com>
2026-04-03 10:00:00 +0100

    Simplification basique du modele utilisateur

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,95 +2,84 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-#[Fillable([
-    'first_name',
-    'last_name',
-    'email',
-    'password',
-    'bio',
-    'city',
-    'avatar',
-    'credit_balance_minutes',
-    'reputation_score',
-    'account_status',
-])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, HasRoles, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'bio',
+        'city',
+        'avatar',
+        'credit_balance_minutes',
+        'reputation_score',
+        'account_status',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function getNameAttribute()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->first_name.' '.$this->last_name;
     }
 
-    protected function name(): Attribute
-    {
-        return Attribute::get(fn () => trim($this->first_name.' '.$this->last_name));
-    }
-
-    public function skills(): HasMany
+    public function skills()
     {
         return $this->hasMany(Skill::class);
     }
 
-    public function needs(): HasMany
+    public function needs()
     {
         return $this->hasMany(Need::class);
     }
 
-    public function sentExchangeRequests(): HasMany
+    public function sentExchangeRequests()
     {
         return $this->hasMany(ExchangeRequest::class, 'learner_id');
     }
 
-    public function receivedExchangeRequests(): HasMany
+    public function receivedExchangeRequests()
     {
         return $this->hasMany(ExchangeRequest::class, 'helper_id');
     }
 
-    public function sentMessages(): HasMany
+    public function sentMessages()
     {
         return $this->hasMany(Message::class, 'sender_id');
     }
 
-    public function transactions(): HasMany
+    public function transactions()
     {
         return $this->hasMany(Transaction::class);
     }
 
-    public function authoredRatings(): HasMany
+    public function authoredRatings()
     {
         return $this->hasMany(Rating::class, 'author_id');
     }
 
-    public function receivedRatings(): HasMany
+    public function receivedRatings()
     {
         return $this->hasMany(Rating::class, 'target_id');
     }
 
-    public function learnerSessions(): HasManyThrough
+    public function learnerSessions()
     {
         return $this->hasManyThrough(
             LearningSession::class,
@@ -100,7 +89,7 @@ class User extends Authenticatable
         );
     }
 
-    public function helperSessions(): HasManyThrough
+    public function helperSessions()
     {
         return $this->hasManyThrough(
             LearningSession::class,
