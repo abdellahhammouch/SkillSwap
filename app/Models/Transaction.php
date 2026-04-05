8fb3645bfe91754abce167b310ff7636f9978045
Abdellah Hammouch <hammouchabdellah4529@gmail.com>
2026-04-05 14:00:00 +0100

    Simplification du modele Transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
@@ -18,12 +17,12 @@ class Transaction extends Model
         'description',
     ];
 
-    public function learningSession(): BelongsTo
+    public function learningSession()
     {
         return $this->belongsTo(LearningSession::class);
     }
 
-    public function user(): BelongsTo
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
