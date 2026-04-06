8eadc485d6991a1bea1234328647f210bcb0cca0
abdellahhammouch <hammouchabdellah4529@gmail.com>
2026-04-06 08:30:00 +0100

    Ajout du repository des categories

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Category;
+
+class CategoryRepository
+{
+    public function getAll()
+    {
+        return Category::orderBy('name')->get();
+    }
+
+    public function create(array $data)
+    {
+        return Category::create($data);
+    }
+
+    public function update(Category $category, array $data)
+    {
+        $category->update($data);
+
+        return $category;
+    }
+
+    public function delete(Category $category)
+    {
+        return $category->delete();
+    }
+}
