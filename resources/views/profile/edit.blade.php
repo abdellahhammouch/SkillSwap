cec40b4e762008ef4b648fde8fbc6088f033ad7c
Abdellah Hammouch <hammouchabdellah4529@gmail.com>
2026-04-18 12:00:00 +0100

    Ajouter un lien vers le profil public

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -9,6 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
+                    <a href="{{ route('users.show', $user) }}" class="text-sm text-indigo-600 hover:text-indigo-900">
+                        View my public profile
+                    </a>
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
