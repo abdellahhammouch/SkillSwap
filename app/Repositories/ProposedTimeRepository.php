0c1d4877c04ec6f8f532ab004ed84b3d3604daa4
Abdellah Hammouch <hammouchabdellah4529@gmail.com>
2026-04-26 13:02:00 +0100

    Finaliser le depot des creneaux

--- app/Repositories/ProposedTimeRepository.php
+++ app/Repositories/ProposedTimeRepository.php
@@ -7,14 +7,33 @@ use App\Models\ProposedTime;
 
 class ProposedTimeRepository
 {
+    public function getPendingForRequest($exchangeRequestId)
+    {
+        return ProposedTime::where('exchange_request_id', $exchangeRequestId)
+            ->where('status', 'pending')
+            ->orderBy('start_at')
+            ->get();
+    }
+
+    public function getNextProposalGroup($exchangeRequestId)
+    {
+        return (int) ProposedTime::where('exchange_request_id', $exchangeRequestId)->max('proposal_group') + 1;
+    }
+
     public function create(ExchangeRequest $exchangeRequest, array $data)
     {
         return $exchangeRequest->proposedTimes()->create($data);
     }
 
-    public function unselectTimesForRequest($exchangeRequestId)
+    public function unselectTimesForRequest($exchangeRequestId, $proposalGroup = null)
     {
-        return ProposedTime::where('exchange_request_id', $exchangeRequestId)
+        $query = ProposedTime::where('exchange_request_id', $exchangeRequestId);
+
+        if ($proposalGroup !== null) {
+            $query->where('proposal_group', $proposalGroup);
+        }
+
+        return $query
             ->update([
                 'is_selected' => false,
             ]);
@@ -23,9 +42,38 @@ class ProposedTimeRepository
     public function select(ProposedTime $proposedTime)
     {
         $proposedTime->update([
+            'status' => 'selected',
             'is_selected' => true,
         ]);
 
         return $proposedTime;
+    }
+
+    public function markOtherTimesAsDenied($exchangeRequestId, $proposalGroup, $selectedProposedTimeId)
+    {
+        return ProposedTime::where('exchange_request_id', $exchangeRequestId)
+            ->where('proposal_group', $proposalGroup)
+            ->where('id', '!=', $selectedProposedTimeId)
+            ->update([
+                'status' => 'denied',
+                'is_selected' => false,
+            ]);
+    }
+
+    public function markGroupAsDenied($exchangeRequestId, $proposalGroup)
+    {
+        return ProposedTime::where('exchange_request_id', $exchangeRequestId)
+            ->where('proposal_group', $proposalGroup)
+            ->update([
+                'status' => 'denied',
+                'is_selected' => false,
+            ]);
+    }
+
+    public function getLatestPendingGroup($exchangeRequestId)
+    {
+        return ProposedTime::where('exchange_request_id', $exchangeRequestId)
+            ->where('status', 'pending')
+            ->max('proposal_group');
     }
 }
